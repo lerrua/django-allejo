895d9df5e9bcd2d6b0627dc3d9bfe7b73f665e1c
Igor P. Leroy <ip.leroy@gmail.com>
2015-03-10 20:19:56 -0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,7 @@ import re
 import sys
 
 
-version_text = '0.0.0'
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, version_text, re.M)
-if mo:
-    version = mo.group(1)
-else:
-    raise RuntimeError(
-        "Unable to find version string in %s." % ('setup.py'))
-
-
+version = '0.1.0'
 name = 'django-allejo'
 package = 'allejo'
 description = ''
@@ -23,19 +14,6 @@ author = 'Igor P. Leroy'
 author_email = 'ip.leroy@gmail.com'
 license = 'BSD'
 install_requires = []
-
-
-if sys.argv[-1] == 'publish':
-    code = os.system("python setup.py sdist upload")
-    if code:
-        sys.exit()
-
-    args = {'version': version}
-    print "You probably want to also tag the version now:"
-    print "  git tag -a %(version)s -m 'version %(version)s'" % args
-    print "  git push --tags"
-    sys.exit()
-
 
 setup(
     name=name,
@@ -48,7 +26,7 @@ setup(
     include_package_data=True,
     description=description,
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
